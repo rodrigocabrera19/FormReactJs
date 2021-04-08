d121b898c77c9a883496e16f7cbdb75790b7441f
rodrigocabrera19 <rodrigod33d@gmail.com>
2021-04-08 10:25:25 -0300

    agregando refs para enviar state de hijo a padre

--- src/form/FormRow.js
+++ src/form/FormRow.js
@@ -1,18 +1,19 @@
-import { useState } from 'react';
+import { useState, forwardRef, useImperativeHandle  } from 'react';
+import React from 'react';
 
-export default function FormRow({label, type}){
-    const [inputValue, setInputValue] = useState('')
-
+export default React.forwardRef(function FormRow(props, ref, label, type){
+    /*Agregando un useImperativeHandler para pasar estado de hijo a padre*/
+    const [inputValue, setInputValue] = useState('');
+    React.useImperativeHandler((ref) => ({
+    getSearchState: () => inputValue,
+    }));
     const onChange = (e) => {
-        setInputValue(e.target.value)
-        
-    }
-    
-
+    setInputValue(e.target.value);
+    };
     return(
         <div>
             <label>{label}</label>
             <input type={type} value={inputValue} onChange={onChange}/>
         </div>
     )
-}
+});
